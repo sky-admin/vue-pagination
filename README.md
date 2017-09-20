33df2b06b01e9e9f72aa9343adb9b4237179813d
lihuanyu <lihuanyu@didichuxing.com>
2017-09-20 15:56:56 +0800

    add vue-cli's webpack template

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# vue-pagination
-Vue分页组件，尝试发个NPM包。
+# vue-simple-pagination
+
+> 一个简单的Vue分页组件
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-page :current-page="currentPage" :total-page="totalPage" @change-page="changePage"></v-page>
+  </div>
+</template>
+
+<script>
+  import vPage from './components/Pagination.vue'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        currentPage: 0,
+        totalPage: 10
+      }
+    },
+    methods: {
+      changePage (newPage) {
+        this.currentPage = newPage
+      }
+    },
+    components: {
+      vPage
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
